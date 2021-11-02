e1be69cef2959dfcd8ba3961b8072707421b8c7d
Federico GR <fedegr77@gmail.com>
2021-11-02 18:17:14 -0300

    Se agrega index + solicitudAfiliacion

---
+++ js/loginAfiliacion.js
@@ -0,0 +1,8 @@
+//Declaración de variables
+var contenedor_register = document.querySelector(".formulario");
+var formulario_register = document.querySelector(".formulario__register");
+
+
+function register {
+
+}
